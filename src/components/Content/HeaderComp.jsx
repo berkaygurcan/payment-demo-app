5776793832c84cbe9032e4781a094ab7db70a136
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-04-10 18:29:32 +0300

    Logo was added

--- src/components/Content/HeaderComp.jsx
+++ src/components/Content/HeaderComp.jsx
@@ -1,17 +1,32 @@
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar } from "antd";
+import { Avatar, Image } from "antd";
 import Text from "antd/lib/typography/Text";
+import Logo from "./logo.png"
 
 import React from "react";
 
 const HeaderComp = () => {
   return (
-    <header id="main-header" style={{ display: "flex", justifyContent: "space-between"}} >
+    <header
+      id="main-header"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
       <div>
-        Logo gelicek buraya
+        <Image
+        style={{objectFit: "cover"}}
+          width={100}
+          src = {Logo}
+        />
       </div>
 
-      <div style={{ display: "flex", justifyContent: "space-around", alignItems:"center",gap:10}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          gap: 10,
+        }}
+      >
         <Avatar size="small" icon={<UserOutlined />} />
         <Text>Ant Design (default)</Text>
       </div>
